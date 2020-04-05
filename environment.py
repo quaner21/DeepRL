25c18bac27ee307c66d95ea0c2b99560347fcf14
tding <tding@blizzard.com>
2020-04-05 16:21:44 -0700

    initial commit

---
+++ environment.py
@@ -0,0 +1,73 @@
+import tensorflow as tf
+import numpy as np
+import random
+import math
+import matplotlib.pyplot as plt
+
+
+class Environment:
+
+    def __init__(self):
+        self.total_T = 1440  # 30s interval for 12h
+        self.min_battery = 0.0
+        self.max_battery = 100.0  # mAh
+        self.event_prob = 0.04
+        self.event_span = 10
+        self.event_detect_reward = 10.0
+
+        self.action_space = ['I', 'S', 'H']
+
+        self.time = 0
+        self.battery = np.random.uniform(self.min_battery, self.max_battery)
+        self.event = False
+        self.event_counter = None
+        self.state = (self.battery, self.time)
+
+    def step(self, action):
+        state = self.state
+        battery, time = state
+
+        if action == 'I':
+            battery += -10 * 30 / 3600
+        elif action == 'S':
+            battery += -30 * 30 / 3600
+        elif action == 'H':
+            battery += 75 * 30 / 3600
+
+        battery = min(max(battery, self.min_battery), self.max_battery)
+        time += 1
+        self.state = (battery, time)
+
+        if self.event == False and self.event_counter is None:
+            event_judge = np.random.uniform(0, 1)
+            if event_judge < self.event_prob:
+                self.event_counter = 0
+                self.event = True
+        if self.event == True and self.event_counter <= self.event_span:
+            self.event_counter += 1
+            self.event = True
+        if self.event == True and self.event_counter > self.event_span:
+            self.event_counter = None
+            self.event = False
+
+        done = battery == self.min_battery \
+               or time == self.total_T
+        done = bool(done)
+
+        if not done:
+            reward = 1.0
+            if self.event == True and action == 'S':
+                reward += self.event_detect_reward
+        else:
+            reward = 0.0
+
+        return np.array(self.state), reward, done
+
+    def reset(self):
+        self.time = 0
+        self.battery = np.random.uniform(self.min_battery, self.max_battery)
+        self.event = False
+        self.event_counter = None
+        self.state = (self.battery, self.time)
+
+        return np.array(self.state)
---
+++ main.py
@@ -0,0 +1,27 @@
+from environment import Environment
+from agent import DQNAgent
+
+if __name__ == "__main__":
+    env = Environment()
+    state_size = 2
+    action_size = 3
+    agent = DQNAgent(state_size, action_size)
+    done = False
+    batch_size = 32
+
+    for e in range(EPISODES):
+        state = env.reset()
+        state = np.reshape(state, [1, state_size])
+        for time in range(1440):
+            action = agent.act(state)
+            next_state, reward, done = env.step(action)
+            reward = reward if not done else -10
+            next_state = np.reshape(next_state, [1, state_size])
+            agent.memorize(state, action, reward, next_state, done)
+            state = next_state
+            if done:
+                print("episode: {}/{}, score: {}, e: {:.2}"
+                      .format(e, EPISODES, time, agent.epsilon))
+                break
+            if len(agent.memory) > batch_size:
+                agent.replay(batch_size)
