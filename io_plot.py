09e5126304543829b749d655ec81e10fcdf82c81
Qiyuan Liu <quaner21@users.noreply.github.com>
2020-04-27 21:57:53 -0700

    save data to .csv and plot

---
+++ io_plot.py
@@ -0,0 +1,32 @@
+import matplotlib.pyplot as plt
+import csv
+
+
+def save_to_file(array, filename, writetype):
+    with open(filename, writetype) as f:
+        csv.writer(f, delimiter=',').writerow(array)
+
+
+def plot_and_save(array, xname, yname, plotname):
+    fig = plt.figure()
+    plt.plot(list(range(len(array))), array)
+    plt.xlabel(xname)
+    plt.ylabel(yname)
+    plt.savefig(plotname)
+    plt.close(fig)
+
+
+def array_pad_zero(array, xlimit):
+    arr_len = len(array)
+    if (xlimit > arr_len):
+        array.append([0 for i in range(xlimit - arr_len)])
+    return array
+
+
+def read_plot_save(filename, xlimit, xname, yname, plotname):
+    with open(filename, newline='') as f:
+        data = list(csv.reader(f))
+    index = 0
+    for array in data:
+        plot_and_save(array_pad_zero(list(map(float, array)), xlimit), xname, yname, plotname + str(index) + '.png')
+        index += 1
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from environment import Environment
 from agent import DQNAgent
 import numpy as np
-import matplotlib.pyplot as plt
+import io_plot as iop
 
 EPISODES = 1000
 
@@ -13,21 +13,28 @@ if __name__ == "__main__":
     done = False
     batch_size = 32
 
+    output_episode = []
+
+    # store for all episodes
+    episode_total_detect_num = []
+    episode_total_event_num = []
+    episode_final_battery = []
+    episode_total_reward = []
+    episode_end_time = []
+
     for e in range(EPISODES):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
-        init_battery = state[0]
+        init_battery = state[0][0]
         event_num = 0
         detect_num = 0
         total_reward = 0
-        output_episode = []
-        output_detect_num = []
-        output_event_num = []
-        output_final_battery = []
-        output_endtime = []
-        output_total_reward = []
 
-        for time in range(1440):
+        step_battery = []
+        step_detect_num = []
+        step_event_num = []
+
+        for time in range(env.total_T):
             action = agent.act(state)
             next_state, reward, event, done = env.step(action)
             if event == True:
@@ -35,14 +42,36 @@ if __name__ == "__main__":
                 if action == 1:  # action_list = {'I', 'S', 'H'}
                     detect_num += 1
             total_reward += reward
+
+            if e == 0 or e == EPISODES - 1:
+                step_battery.append(state[0][0])
+                step_detect_num.append(detect_num)
+                step_event_num.append(event_num)
+
             next_state = np.reshape(next_state, [1, state_size])
             agent.memorize(state, event, action, reward, next_state, done)
             state = next_state
             if done:
-                final_battery = state[0]
-                print("episode: {}/{}, detect event: {}/{}, initial battery: {:.2f}, final battery: {:.2f}, end time: {}, total reward: {:.2f}"
-                      .format(e, EPISODES, detect_num, event_num, init_battery[0], final_battery[0], time, total_reward))
+                final_battery = state[0][0]
+                print(
+                    "episode: {}/{}, detect event: {}/{}, initial battery: {:.2f}, final battery: {:.2f}, end time: {}, total reward: {:.2f}"
+                        .format(e, EPISODES, detect_num, event_num, init_battery, final_battery, time, total_reward))
+                episode_total_detect_num.append(detect_num)
+                episode_total_event_num.append(event_num)
+                episode_final_battery.append(final_battery)
+                episode_total_reward.append(total_reward)
+                episode_end_time.append(time)
+                if e == 0 or e == EPISODES - 1:
+                    iop.save_to_file(step_battery, './data/step_battery.csv', 'a')
+                    iop.save_to_file(step_detect_num, './data/step_detect_num.csv', 'a')
+                    iop.save_to_file(step_event_num, './data/step_event_num.csv', 'a')
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
+
+    iop.save_to_file(episode_total_detect_num, './data/episode_total_detect_num.csv', 'w')
+    iop.save_to_file(episode_total_event_num, './data/episode_total_event_num.csv', 'w')
+    iop.save_to_file(episode_final_battery, './data/episode_final_battery.csv', 'w')
+    iop.save_to_file(episode_total_reward, './data/episode_total_reward.csv', 'w')
+    iop.save_to_file(episode_end_time, './data/episode_end_time.csv', 'w')
 
